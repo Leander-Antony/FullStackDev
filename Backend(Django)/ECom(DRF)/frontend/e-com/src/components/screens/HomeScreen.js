4ad4750a9043349adff574f95af18cd9019bef35
Leander-Antony <leander.antony2023@gmail.com>
2024-12-12 16:54:19 +0530

    session 5 completed

--- Backend(Django)/ECom(DRF)/frontend/e-com/src/components/screens/HomeScreen.js
+++ Backend(Django)/ECom(DRF)/frontend/e-com/src/components/screens/HomeScreen.js
@@ -3,29 +3,36 @@ import { Container } from "react-bootstrap";
 import axios from "axios";
 import { Row, Col, Card } from "react-bootstrap";
 import Product from "../Product";
-
+import { listProducts } from "../../actions/productsActions";
+import { useDispatch, useSelector } from "react-redux";
 
 function HomeScreen() {
-  const [products, setProducts] = useState([]);
+  const dispatch = useDispatch();
+  const productList = useSelector((state) => state.productList);
+  const { error, loading, products } = productList;
 
   useEffect(() => {
-    async function fetchproducts() {
-      const { data } = await axios.get("/products");
-      setProducts(data);
-    }
-    fetchproducts();
-  }, []);
+    dispatch(listProducts());
+  }, [dispatch]);
   return (
     <Container>
       <br />
       <h1>products</h1>
-      <Row>
+      {
+        loading ? (
+          <h2>Loading........</h2>
+        ):error ? (
+          <h3>{error}</h3>
+        ):(
+          <Row>
         {products.map((product) => (
           <Col key={product._id} sm={12} md={6} lg={4} cl={3}>
             <Product product={product} />
           </Col>
         ))}
       </Row>
+        )
+      }
     </Container>
   );
 }
